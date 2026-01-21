e64eb3c58bc59fe08ce5b6932725004cc896ee0a
Roc Lai <laipeng668@qq.com>
2026-01-21 16:18:48 +0800

    添加kexec支持

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -17,6 +17,10 @@ sed -i "s#_('Firmware Version'), (L\.isObject(boardinfo\.release) ? boardinfo\.r
 # 移除luci-app-attendedsysupgrade软件包
 sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
+# 添加kexec支持aarch64架构
+sed -i -e 's/@(armeb||arm||i386||x86_64||powerpc64||mipsel||mips)/@(armeb||arm||aarch64||i386||x86_64||powerpc64||mipsel||mips)/' \
+    -e 's/@(i386||x86_64||arm)/@(i386||x86_64||arm||aarch64)/' package/boot/kexec-tools/Makefile
+
 # 调整NSS驱动q6_region内存区域预留大小（ipq6018.dtsi默认预留85MB，ipq6018-512m.dtsi默认预留55MB，带WiFi必须至少预留54MB，以下分别是改成预留16MB、32MB、64MB和96MB）
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x01000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x02000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
