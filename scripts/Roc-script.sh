53d199b1277149a875e2bb05374ef15166b65475
ryan-lee2009 <ryan.liou@outlook.com>
2026-01-22 09:24:20 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -1,90 +1,89 @@
-# 修改默认IP & 固件名称 & 编译署名和时间
+#!/bin/bash
+set -e
+
+echo "=== IPQ6000 极限压榨版：PassWall + nikki ==="
+
+#-------------------------------------------------
+# 1. 基础信息
+#-------------------------------------------------
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='IPQ6000'/g" package/base-files/files/bin/config_generate
+
+#-------------------------------------------------
+# 2. LuCI 固件署名
+#-------------------------------------------------
 sed -i "s#_('Firmware Version'), (L\.isObject(boardinfo\.release) ? boardinfo\.release\.description + ' / ' : '') + (luciversion || ''),# \
             _('Firmware Version'),\n \
             E('span', {}, [\n \
                 (L.isObject(boardinfo.release)\n \
                 ? boardinfo.release.description + ' / '\n \
-                : '') + (luciversion || '') + ' / ',\n \
-            E('a', {\n \
-                href: 'https://github.com/laipeng668/openwrt-ci-roc/releases',\n \
-                target: '_blank',\n \
-                rel: 'noopener noreferrer'\n \
-                }, [ 'Built by Roc $(date "+%Y-%m-%d %H:%M:%S")' ])\n \
-            ]),#" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+                : '') + (luciversion || '') + ' / Built for IPQ6000'\n \
+            ]),#" \
+feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 
-# 移除luci-app-attendedsysupgrade软件包
-sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+#-------------------------------------------------
+# 3. 移除 attendedsysupgrade
+#-------------------------------------------------
+sed -i "/attendedsysupgrade/d" $(find feeds/luci/collections/ -name Makefile)
 
-# 添加kexec支持aarch64架构
+#-------------------------------------------------
+# 4. kexec 支持 aarch64（IPQ6000 必须）
+#-------------------------------------------------
 sed -i -e 's/@(armeb||arm||i386||x86_64||powerpc64||mipsel||mips)/@(armeb||arm||aarch64||i386||x86_64||powerpc64||mipsel||mips)/' \
-    -e 's/@(i386||x86_64||arm)/@(i386||x86_64||arm||aarch64)/' package/boot/kexec-tools/Makefile
+       -e 's/@(i386||x86_64||arm)/@(i386||x86_64||arm||aarch64)/' \
+package/boot/kexec-tools/Makefile
 
-# 调整NSS驱动q6_region内存区域预留大小（ipq6018.dtsi默认预留85MB，ipq6018-512m.dtsi默认预留55MB，带WiFi必须至少预留54MB，以下分别是改成预留16MB、32MB、64MB和96MB）
-# sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x01000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
-# sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x02000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
-# sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x04000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
-# sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x06000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
+#-------------------------------------------------
+# 5. 删除不需要的软件
+#-------------------------------------------------
+rm -rf \
+feeds/luci/applications/luci-app-openclash \
+feeds/luci/applications/luci-app-homeproxy \
+feeds/luci/applications/luci-app-attendedsysupgrade \
+feeds/packages/net/open-app-filter \
+feeds/packages/net/ariang \
+feeds/packages/net/frp \
+feeds/packages/lang/golang
 
-# 移除要替换的包
-rm -rf feeds/luci/applications/luci-app-argon-config
-rm -rf feeds/luci/applications/luci-app-wechatpush
-rm -rf feeds/luci/applications/luci-app-appfilter
-rm -rf feeds/luci/applications/luci-app-frpc
-rm -rf feeds/luci/applications/luci-app-frps
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/packages/net/open-app-filter
-rm -rf feeds/packages/net/ariang
-rm -rf feeds/packages/net/frp
-rm -rf feeds/packages/lang/golang
+#-------------------------------------------------
+# 6. nikki（官方 sing-box）
+#-------------------------------------------------
+git clone --depth=1 https://github.com/nikkinikki-org/luci-app-nikki package/luci-app-nikki
 
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
+#-------------------------------------------------
+# 7. PassWall：极限精简核心
+#-------------------------------------------------
+
+# ❌ 移除 feeds 中会被 PassWall 接管或浪费内存的核心
+rm -rf feeds/packages/net/{ \
+xray-core,v2ray-geodata, \
+chinadns-ng,dns2socks,ipt2socks,microsocks, \
+naiveproxy,shadowsocks-libev,shadowsocks-rust, \
+shadowsocksr-libev,simple-obfs,tcping, \
+trojan-plus,tuic-client,hysteria, \
+v2ray-plugin,xray-plugin,geoview,shadow-tls \
 }
 
-# ariang & frp & WolPlus & Argon & Aurora & Go & OpenList & Lucky & wechatpush & OpenAppFilter & 集客无线AC控制器 & 雅典娜LED控制
-git_sparse_clone ariang https://github.com/laipeng668/packages net/ariang
-git_sparse_clone frp https://github.com/laipeng668/packages net/frp
-mv -f package/frp feeds/packages/net/frp
-git_sparse_clone frp https://github.com/laipeng668/luci applications/luci-app-frpc applications/luci-app-frps
-mv -f package/luci-app-frpc feeds/luci/applications/luci-app-frpc
-mv -f package/luci-app-frps feeds/luci/applications/luci-app-frps
-git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
-git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora feeds/luci/themes/luci-theme-aurora
-git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config feeds/luci/applications/luci-app-aurora-config
-git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
-git clone --depth=1 https://github.com/sbwml/luci-app-openlist2 package/openlist2
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
-git clone --depth=1 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
-git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
-git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
-chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+# ✔ 保留 feeds 官方 sing-box（给 nikki 用）
 
-### PassWall & OpenClash ###
-
-# 移除 OpenWrt Feeds 自带的核心库
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+# PassWall 核心
 git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 
-# 移除 OpenWrt Feeds 过时的LuCI版本
+# LuCI
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall package/luci-app-passwall
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2 package/luci-app-passwall2
-git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-openclash
 
-# 清理 PassWall 的 chnlist 规则文件
-echo "baidu.com"  > package/luci-app-passwall/luci-app-passwall/root/usr/share/passwall/rules/chnlist
+# ❌ 不要 passwall2（省 20MB+）
+rm -rf feeds/luci/applications/luci-app-passwall2
 
+# 精简 chnlist
+echo "baidu.com" > \
+package/luci-app-passwall/luci-app-passwall/root/usr/share/passwall/rules/chnlist
+
+#-------------------------------------------------
+# 8. feeds
+#-------------------------------------------------
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+echo "=== IPQ6000 极限压榨完成 ==="
